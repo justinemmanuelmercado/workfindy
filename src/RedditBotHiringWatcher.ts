1559cf30e5dc3378e59b0923a1557aab5203b4b8
Justin Emmanuel Mercado <ej@ejmercado.com>
2023-01-28 21:49:35 +0800

    Adds a hiring listener for general hiring posts

--- src/RedditBotHiringWatcher.ts
+++ src/RedditBotHiringWatcher.ts
@@ -5,57 +5,48 @@ import { Watcher } from "./abstract/Watcher";
 import { matchingWords } from "./filters";
 import { RedditPlatform } from "./RedditPlatform";
 
-export class RedditBotForHireDeveloperWatcher implements Watcher {
-  id = "RedditBotTestingGround";
+export class RedditBotHiringWatcher implements Watcher {
+  id: string = "RedditBotHiringWatcher";
   config: any = {};
   platform;
   channels: Channel[] = [];
-  keywords: string[] = [
-    "developer",
-    "web",
-    "angular",
-    "react",
-    "reactjs",
-    "node",
-    "nodejs",
-    "javascript",
-    "python",
-    "java",
-    "ruby",
-    "c#",
-    "c++",
-    "html",
-    "css",
-    "typescript",
-    "php",
-    "ruby",
-    "rust",
-    "golang",
-    "flutter",
-    "vue",
-    "swift",
-    "kotlin",
-    "react native",
-    "full stack",
-  ];
+  keywordsPostTitle?: string[];
+  keywordsPostBody?: string[];
+  keywordsCommentBody?: string[];
+  subreddit: string;
 
-  constructor(platform: RedditPlatform) {
+  constructor(
+    platform: RedditPlatform,
+    options: {
+      keywordsPostTitle?: string[];
+      keywordsPostBody?: string[];
+      keywordsCommentBody?: string[];
+      channels: Channel[];
+      subreddit: string;
+    }
+  ) {
     this.platform = platform;
-    this.config.subreddit = "forhire";
+
+    this.keywordsPostTitle = options.keywordsPostTitle;
+    this.keywordsPostBody = options.keywordsPostBody;
+    this.keywordsCommentBody = options.keywordsCommentBody;
+    this.channels = options.channels;
+    this.subreddit = options.subreddit;
+
     this.connect();
   }
 
   async connect() {
     const postStream = new SubmissionStream(this.platform.snoowrap, {
-      subreddit: this.config.subreddit,
+      subreddit: this.subreddit,
       limit: 10,
-      pollTime: 10000,
+      pollTime: 30000,
     });
 
     const commentStream = new CommentStream(this.platform.snoowrap, {
-      subreddit: this.config.subreddit,
+      subreddit: this.subreddit,
       limit: 10,
-      pollTime: 10000,
+      pollTime: 30000,
     });
 
     this.config.streams = [postStream, commentStream];
@@ -65,8 +56,14 @@ export class RedditBotForHireDeveloperWatcher implements Watcher {
     this.config.streams.forEach((stream: CommentStream | SubmissionStream) => {
       stream.on("item", async (item: Comment | Submission) => {
         if (isComment(item)) {
+          if(!this.keywordsCommentBody) {
+            return;
+          }
           await this.handleComment(item);
         } else {
+          if(!this.keywordsPostTitle && !this.keywordsPostBody) {
+            return;
+          }
           await this.handleSubmission(item);
         }
       });
@@ -74,11 +71,15 @@ export class RedditBotForHireDeveloperWatcher implements Watcher {
   }
 
   async handleComment(item: Comment) {
-    const matchesBody = matchingWords(this.keywords, item.body);
+    if(!this.keywordsCommentBody) {
+      return;
+    }
+
+    const matchesBody = matchingWords(this.keywordsCommentBody, item.body);
     let message = "\n\n";
     message += "\n---------------------------------------------------------";
     message += "\n**Found a match in a comment:**";
-    const title = "NEW MATCHED COMMENT IN /r/forhire";
+    const title = "NEW MATCHED COMMENT IN /r/" + this.subreddit;
     if (matchesBody.length > 0) {
       message += `\n[LINK HERE](https://www.reddit.com/${item.permalink})`;
       message += "\n---------------------------------------------------------";
@@ -87,27 +88,29 @@ export class RedditBotForHireDeveloperWatcher implements Watcher {
         message += `\n**${match}**`;
       });
       message += "\n\n**Body:**\n```" + item.body + "```";
-      for (const channel of this.channels) {
-        channel.sendMessage({
-          title: title,
-          body: message,
-        });
-      }
+      this.sendToChannels({
+        title,
+        body: message,
+      });
     }
   }
 
-  async handleSubmission(item: Submission) {
-    const matchesHiring = matchingWords(["HIRING"], item.title);
-    // Post is not hiring, so we don't care about it.
-    if (matchesHiring.length === 0) {
-      return;
+  sendToChannels = async (message: { title: string; body: string }) => {
+    for (const channel of this.channels) {
+      await channel.sendMessage({
+        title: message.title,
+        body: message.body,
+      });
     }
+  };
 
-    const matchesTitle = matchingWords(this.keywords, item.title);
-    const matchesBody = matchingWords(this.keywords, item.selftext);
+  async handleSubmission(item: Submission) {
+    const matchesTitle = matchingWords(this.keywordsPostTitle ?? [], item.title);
+    const matchesBody = matchingWords(this.keywordsPostBody ?? [], item.selftext);
+    console.log("L ", matchesTitle, item.title);
     let message = "\n\n";
     message += "\n---------------------------------------------------------";
-    const title = "NEW MATCHED POST IN /r/forhire";
+    const title = "NEW MATCHED POST IN /r/" + this.subreddit;
     if (matchesTitle.length > 0 || matchesBody.length > 0) {
       message += `\n[LINK HERE](https://www.reddit.com/${item.permalink})`;
       message += "\n---------------------------------------------------------";
@@ -119,12 +122,11 @@ export class RedditBotForHireDeveloperWatcher implements Watcher {
         message += `\n${match}`;
       });
       message += "\n\n**Title:**\n```" + item.title + "```";
-      for (const channel of this.channels) {
-        channel.sendMessage({
-          title,
-          body: message,
-        });
-      }
+
+      this.sendToChannels({
+        title,
+        body: message,
+      });
     }
   }
 
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import { ConsoleChannel } from "./ConsoleChannel";
 import { DiscordChannel } from "./DiscordChannel";
 import { DiscordMedium } from "./DiscordMedium";
 import { RedditBotForHireDeveloperWatcher } from "./RedditBotForHireDeveloperWatcher";
-import { RedditBotTestingGroundWatcher } from './RedditBotTestingGroundWatcher';
+import { RedditBotHiringWatcher } from './RedditBotHiringWatcher';
+import { RedditBotTestingGroundWatcher } from "./RedditBotTestingGroundWatcher";
 import { RedditPlatform } from "./RedditPlatform";
 config();
 
@@ -13,7 +14,7 @@ const logChannel = new ConsoleChannel(new MockMedium());
 const discordMedium = new DiscordMedium();
 
 async function main() {
-  console.log("STARTING...");
+  console.log("STARTING: " + new Date().toLocaleString());
   await discordMedium.connect();
 
   const devDiscordChannel = new DiscordChannel(
@@ -28,21 +29,32 @@ async function main() {
     "Test Channel"
   );
 
+  const redditHiringDiscordChannel = new DiscordChannel(
+    discordMedium,
+    process.env.JOBBYMCJOBFACE_HIRING_CHANNEL_ID || "",
+    "Hiring Channel"
+  );
 
-  
-
-  const redditTestWatcher = new RedditBotTestingGroundWatcher(
-    redditPlatform
-  )
+  const redditTestWatcher = new RedditBotTestingGroundWatcher(redditPlatform);
 
   const redditFHDeveloperWatcher = new RedditBotForHireDeveloperWatcher(
     redditPlatform
   );
 
+  const redditHiringWatcher = new RedditBotHiringWatcher(redditPlatform, {
+    keywordsPostTitle: ["[hiring]"],
+    channels: [redditHiringDiscordChannel, logChannel],
+    subreddit: "forhire",
+  });
+
   redditTestWatcher.channels = [testDiscordChannel, logChannel];
   // redditTestWatcher.listen();
   redditFHDeveloperWatcher.setChannels([devDiscordChannel, logChannel]);
   redditFHDeveloperWatcher.listen();
+  redditHiringWatcher.listen();
+
+  // log out date in the format MM/DD/YYYY HH:MM:SS
+  console.log("STARTED: ", new Date().toLocaleString());
 }
 
 main();
