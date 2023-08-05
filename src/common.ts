87d29f04cc1cdd699586235784c2231ad5597923
Justin Emmanuel Mercado <ej@ejmercado.com>
2023-08-05 22:38:29 +0800

    Adds basic page filtering

--- src/common.ts
+++ src/common.ts
@@ -9,3 +9,20 @@ export const timeAgo = global.timeAgo || new TimeAgo('en-US');
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 if (process.env.NODE_ENV !== "production") global.timeAgo = timeAgo;
+
+type QueryParam = {
+  key: string;
+  value: string | string[];
+};
+
+export function buildQueryString(params: QueryParam[]): string {
+  return params
+    .map((param) => {
+      const key = encodeURIComponent(param.key);
+      const value = Array.isArray(param.value)
+        ? param.value.map((v) => `${key}=${encodeURIComponent(v)}`).join('&')
+        : `${key}=${encodeURIComponent(param.value)}`;
+      return value;
+    })
+    .join('&');
+}
